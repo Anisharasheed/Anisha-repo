59b382154795ca3c3f582b031590bf59234aca3d
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-13 14:22:47 +0530

    Create ThreadHashSet.java

---
+++ ThreadSet/ThreadHashSet.java
@@ -0,0 +1,30 @@
+package Listprog;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+public class ThreadHashSet2 {
+	
+		
+			public static void main(String[] args) {
+				HashSet<Integer> l5 =  new HashSet<>();
+				l5.add(7);
+				l5.add(89);
+				l5.add(45);
+				l5.add(49);
+				l5.add(05);
+				l5.add(35);
+				System.out.println(l5);
+				System.out.println("the size of elements"+l5.size());
+				System.out.println("before removing an element"+l5.size());
+			l5.remove(4);
+			System.out.println("after removing an element"+l5.size());
+			System.out.println(l5);
+	   for(Integer a:l5) {
+		   if(a%7==0)
+			   System.out.println(a);
+	   }
+			
+			}}
+
