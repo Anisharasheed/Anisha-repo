3852f5bdf6b53e2e80aedec5a17e60f324ab1fb6
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-17 00:22:54 +0530

    Create Task9.js

---
+++ Feb14 JavaScript/Task9.js
@@ -0,0 +1,16 @@
+var month ="July";
+var year = 2020;{
+  
+if(year%4==0){
+if(month=="February"){
+ console.log("The year is leap year and the month is  February");
+}
+else{
+  console.log("The year is leap year and the month is not February");
+  
+}
+}
+else{
+  console.log("The year is not a leap year");
+}
+}
