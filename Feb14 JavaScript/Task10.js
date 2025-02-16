cfce2ac36e7a5a431540a720331e638609abae2d
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-17 00:52:07 +0530

    Create Task10.js

---
+++ Feb14 JavaScript/Task10.js
@@ -0,0 +1,18 @@
+var marks = 20
+
+if(marks==100){
+  console.log("Phenomenal!");
+}
+  else if(marks >85){
+     console.log("Excellent");
+  }
+  else if(marks>=75&& marks<=85){
+    console.log("Good Work!");
+  }
+else if(marks>=51&& marks<=70){
+  console.log("Practice well");
+}
+else{
+  console.log("Hardwork needed");
+}
+
