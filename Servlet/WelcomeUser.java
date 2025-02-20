bd2426f82a57a8d22ec3eaef7365b4672568687e
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-20 23:00:37 +0530

    Create WelcomeUser.java

---
+++ Servlet/WelcomeUser.java
@@ -0,0 +1,36 @@
+package mypack;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/welcome")
+public class WelcomeUser extends HttpServlet { 
+	 
+	 public void doPost(HttpServletRequest request, 
+	 HttpServletResponse response) 
+	 throws ServletException, IOException 
+	 {
+
+	
+	response.setContentType("text/html"); 
+	    PrintWriter pwriter = response.getWriter(); 
+	 
+	    String name=request.getParameter("uname"); 
+	    pwriter.print("Hello "+name+"!");
+	    pwriter.print(" Welcome to Beginnersbook.com");}}
