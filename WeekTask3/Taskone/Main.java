8cbe86e8f7c3e3334191916f39c215e175ab4a7c
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-24 22:00:20 +0530

    Create Main.java

---
+++ WeekTask3/Taskone/Main.java
@@ -0,0 +1,20 @@
+package Task1;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Task 1: Copy Alternative Elements in an Array");
+        CopyAlternateElements.copy();
+
+        System.out.println("\nTask 2: Student Marks Calculation & Sorting");
+        StudentRecords.handleStudentRecords();
+
+        System.out.println("\nTask 3: Compare equals() and deepEquals()");
+        EqualityComparison.compareEqualityMethods();
+
+        System.out.println("\nTask 4: Copy Part of an Array");
+        ArrayCopyDemo.demonstrateArrayCopy();
+
+        System.out.println("\nTask 5: Capitalize Alternate Characters in a String");
+        StringModifier.modifyString("developer");
+    }
+}
