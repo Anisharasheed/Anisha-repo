b063d1268e42a1517e16b8abcac105cfb04ee16e
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-25 08:45:22 +0530

    Create Task1.java

---
+++ WeekTask3/TaskSix/Task1.java
@@ -0,0 +1,8 @@
+package Task1;
+import java.util.Arrays;
+
+public class ArrayComparator {
+    public static <T> boolean compareArrays(T[] arr1, T[] arr2) {
+        return Arrays.equals(arr1, arr2);
+    }
+}
