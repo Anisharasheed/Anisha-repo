d29c79ee38b1add7cd8e2d20d32a4f6cd33df068
Anisharasheed <anishahakunamatata4208@gmail.com>
2025-02-25 08:51:29 +0530

    Create Task1.java

---
+++ WeekTask3/TaskEight/Task1.java
@@ -0,0 +1,31 @@
+package Task1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/student";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Monii@24";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+                System.out.println("Database connection closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
